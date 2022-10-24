cbc69f5ec8bc305a1ed99d879cef91454381369e
Fazhrul Islam Sadip <86272587+sadipnsu@users.noreply.github.com>
2022-10-24 14:32:31 +0600

    Dynamic list added

--- src/app/page-list/page-list.component.ts
+++ src/app/page-list/page-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { TableListService } from '../table-list.service';
 
 @Component({
   selector: 'app-page-list',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageListComponent implements OnInit {
 
-  constructor() { }
+  tableList : any =[];
+  pageName : string ="";
+  Id: string | null = null;
+  numId : number = -1;
+
+  constructor(private router: Router, private _tableService : TableListService, private route: ActivatedRoute ) { }
 
   ngOnInit(): void {
-  }
 
+    this.tableList = this._tableService.getTableList();
+    this.Id = this.route.snapshot.paramMap.get('id');
+    this.numId = Number(this.Id);
+    for (var i in this.tableList){
+      if (this.tableList[i].sl == (this.numId) ){
+        this.pageName = this.tableList[i].name;
+      }
+    };
+
+  }
+pageBack(){
+    this.router.navigate(['']);
+  }
 }
--- src/app/table-list.service.ts
+++ src/app/table-list.service.ts
@@ -9,11 +9,13 @@ export class TableListService {
 
   getTableList(){
     return [
-      {"sl": 1, "name": "test1"},
-      {"sl": 2, "name": "test2"},
-      {"sl": 3, "name": "test3"},
-      {"sl": 4, "name": "test4"},
-      {"sl": 5, "name": "test5"},
+      {"sl": 1, "name": "Test1"},
+      {"sl": 2, "name": "Test2"},
+      {"sl": 3, "name": "Test3"},
+      {"sl": 4, "name": "Test4"},
+      {"sl": 5, "name": "Test5"},
+      {"sl": 6, "name": "Test6"},
+      {"sl": 7, "name": "Test7"},
     ]
   }
 }
